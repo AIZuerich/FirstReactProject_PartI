dd9c0931b11986058b9b6de9bcd65dbc498539f1
A I <ai_zuerich@mein.gmx>
2020-11-18 22:31:35 +0000

    few things

--- src/about.js
+++ src/about.js
@@ -22,7 +22,23 @@ function About(){
 
     <article id="about_me">
     <div class="main">
-    <h1>About Anti Spam</h1>
+    <h1>About</h1>
+    <div id="about_me_div">
+    <p>This bot is a fun project. I was bothered by spam messages, so I decided to make a bot that filters and deletes them. 
+<br/>
+<br/>
+Anti Spam function: My bot scans all messages and attachments for speed. If they are posted too fast by a user, they will be deleted. That's not all, even slow spams are deleted if the messages have the same length. The bot also scans the messages for NSFW links and other annoying links. Unfortunately this function is still very limited. One thing: Admins are ignored by the Anti Spam Bot.
+<br/>
+<br/>
+The Raid function: In the channel named welcome a password is required for the new users. Only the admin can activate this command.
+<br/>
+<br/>
+Plans: Better NSFW filter. Anti Virus Scanner for attachments.
+<br/>
+<br/>
+News: We are reaching our limits. The bot is growing too fast. We try to find a solution.
+</p>
+    </div>
     </div>
     </article>
   )
